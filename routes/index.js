c6a986167b6485d6a938a2e2b2e109b39b56324e
thzero <thzero@users.noreply.github.com>
2022-04-16 09:09:28 -0500

    json output content type handling

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,7 @@ class RegistryRoute extends BaseRoute {
 			async (ctx, next) => {
 				const service = this._injector.getService(Constants.InjectorKeys.SERVICE_RESOURCE_DISCOVERY);
 				const response = (await service.deregister(ctx.correlationId, ctx.params.name)).check(ctx);
-				ctx.body = Utility.stringify(response);
+				this._jsonResponse(ctx, Utility.stringify(response));
 			}
 		);
 
@@ -34,7 +34,7 @@ class RegistryRoute extends BaseRoute {
 			async (ctx, next) => {
 				const service = this._injector.getService(Constants.InjectorKeys.SERVICE_RESOURCE_DISCOVERY);
 				const response = (await service.listing(ctx.correlationId, ctx.request.body)).check(ctx);
-				ctx.body = Utility.stringify(response);
+				this._jsonResponse(ctx, Utility.stringify(response));
 			}
 		);
 
@@ -45,7 +45,7 @@ class RegistryRoute extends BaseRoute {
 			async (ctx, next) => {
 				const service = this._injector.getService(Constants.InjectorKeys.SERVICE_RESOURCE_DISCOVERY);
 				const response = (await service.register(ctx.correlationId, ctx.request.body)).check(ctx);
-				ctx.body = Utility.stringify(response);
+				this._jsonResponse(ctx, Utility.stringify(response));
 			}
 		);
 
@@ -54,7 +54,7 @@ class RegistryRoute extends BaseRoute {
 			async (ctx, next) => {
 				const service = this._injector.getService(Constants.InjectorKeys.SERVICE_RESOURCE_DISCOVERY);
 				const response = (await service.get(ctx.correlationId, ctx.params.name)).check(ctx);
-				ctx.body = Utility.stringify(response);
+				this._jsonResponse(ctx, Utility.stringify(response));
 			}
 		);
 	}
