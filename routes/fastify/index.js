13144fdc56e6d4a125ae0b1d4693feb66817b042
thzero <thzero@users.noreply.github.com>
2022-04-18 16:05:56 -0500

    updates for fastify

--- routes/fastify/index.js
+++ routes/fastify/index.js
@@ -11,7 +11,6 @@ class RegistryRoute extends BaseRoute {
 
 	async init(injector, app, config) {
 		await super.init(injector, app, config);
-		// router.serviceResourceDiscovery = injector.getService(Constants.InjectorKeys.SERVICE_RESOURCE_DISCOVERY);
 		this._inject(app, injector, Constants.InjectorKeys.SERVICE_RESOURCE_DISCOVERY, Constants.InjectorKeys.SERVICE_RESOURCE_DISCOVERY);
 	}
 
@@ -21,24 +20,24 @@ class RegistryRoute extends BaseRoute {
 
 	_initializeRoutes(router) {
 		router.delete(this._join('/:name'),
+			// eslint-disable-next-line
 			async (request, reply) => {
-				// const service = this._injector.getService(Constants.InjectorKeys.SERVICE_RESOURCE_DISCOVERY);
 				const response = (await router[Constants.InjectorKeys.SERVICE_RESOURCE_DISCOVERY].deregister(request.correlationId, request.params.name)).check(request);
 				this._jsonResponse(reply, Utility.stringify(response));
 			}
 		);
 
 		router.post(this._join('/listing'),
+			// eslint-disable-next-line
 			async (request, reply) => {
-				// const service = this._injector.getService(Constants.InjectorKeys.SERVICE_RESOURCE_DISCOVERY);
 				const response = (await router[Constants.InjectorKeys.SERVICE_RESOURCE_DISCOVERY].listing(request.correlationId, request.body)).check(request);
 				this._jsonResponse(reply, Utility.stringify(response));
 			}
 		);
 
 		router.post(this._join('/'),
+			// eslint-disable-next-line
 			async (request, reply) => {
-				// const service = this._injector.getService(Constants.InjectorKeys.SERVICE_RESOURCE_DISCOVERY);
 				const response = (await router[Constants.InjectorKeys.SERVICE_RESOURCE_DISCOVERY].register(request.correlationId, request.body)).check(request);
 				this._jsonResponse(reply, Utility.stringify(response));
 			}
@@ -47,7 +46,6 @@ class RegistryRoute extends BaseRoute {
 		router.get(this._join('/:name'),
 			// eslint-disable-next-line
 			async (request, reply) => {
-				// const service = this._injector.getService(Constants.InjectorKeys.SERVICE_RESOURCE_DISCOVERY);
 				const response = (await router[Constants.InjectorKeys.SERVICE_RESOURCE_DISCOVERY].register(request.correlationId, request.params.name)).check(request);
 				this._jsonResponse(reply, Utility.stringify(response));
 			}
